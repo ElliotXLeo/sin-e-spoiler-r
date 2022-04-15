006c21c89d0684576bebc0e727606b123af327cb
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-15 13:42:59 -0500

    component Error404

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import './App.css';
 import MoviesMovieDetails from './components/movies/MoviesMovieDetails';
 import Header from './components/sections/Header';
+import Error404 from './pages/Error404';
 import Movies from './pages/Movies';
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <Route exact path="/estrenos" element={<section className="section animate__animated animate__fadeIn">Estrenos</section>} />
           <Route exact path="/movies/:movieId" element={<MoviesMovieDetails />} />
           <Route exact path="/" element={<Movies />} />
-          {/* <Route path="*" element={<section className="section animate__animated animate__fadeIn">Error 404</section>} /> */}
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </main>
     </Router>
